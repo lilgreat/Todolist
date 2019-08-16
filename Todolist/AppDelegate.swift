6ba61eaa54c5fe444b9f5a8430af8bdb29d75908
gngh <gh>
2019-08-16 17:43:33 +0200

    data persisted using user defaults method

--- Todolist/AppDelegate.swift
+++ Todolist/AppDelegate.swift
@@ -15,30 +15,38 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        print(NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).last! as String)
+        
+        
         return true
+        // gets called when your app gets loaded up.
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+        
+        // Not terminating the progress if the user clicks out for a bit.
+        
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        
+        // happens when the app dissapears of the screen. i. e gets shifted back like you would do with a deck of cards for example.
+        
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+        
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        
+        // when your app is hogging all data the app will be assisinated by the operatingsystem.
+        // or if the user double clicks on the home button an swipes your app up then it will get assinated too.
     }
 
 
--- Todolist/TodolistViewController.swift
+++ Todolist/TodolistViewController.swift
@@ -11,16 +11,29 @@ import UIKit
 class TodolistViewController: UITableViewController {
     
     var itemArray = ["Buy milk", "Dont forget keys", "Close the fridge door"]
+    
+    let defaults = UserDefaults.standard
 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return itemArray.count
+        
+        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
+            itemArray = items
+        }
     }
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    
+    // the three datasource methods:
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int { //<- decides how many rows
+        return itemArray.count
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell { // <-> creates the cell for the tableview
+        
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
         cell.textLabel?.text = itemArray[indexPath.row]
@@ -29,35 +42,40 @@ class TodolistViewController: UITableViewController {
     }
     
     
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
-        //print(itemArray[indexPath.row])
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) { // <- what happens when you select a row.
         
         if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
             tableView.cellForRow(at: indexPath)?.accessoryType = .none
         }
         else {
             tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-            
-        }
+            }
         
-        tableView.deselectRow(at: indexPath, animated: true)
+        
+        tableView.deselectRow(at: indexPath, animated: true) // <- so there is a smooth animation.
         
         }
     
     
-    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+    
+    
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) { // <- what happens when you press the add button
         
         var textField = UITextField()
+        
         
         // you write your todolist item here
         let alert = UIAlertController(title: "Add new Todolist item", message: "", preferredStyle: .alert)
         
-            // the button you press when you are done writing your todolist message
+        
+        // the button you press when you are done writing your todolist message
         let action = UIAlertAction(title: "Add item", style: .default) { (action) in
-            // this is where i'll code the stuff that will happen when the item is appended
+
             
-            self.itemArray.append(textField.text ?? "Be a kinder person")
+            self.itemArray.append(textField.text ?? "Be a kinder person") //<- i set a default value... i know not the most fancy solution ever but so what i hope to annoy some people with my over-cheesiness
+            
+            self.defaults.set(self.itemArray, forKey: "TodoListArray")
             
             // the "magic" method
             self.tableView.reloadData()
@@ -66,7 +84,7 @@ class TodolistViewController: UITableViewController {
         
         alert.addTextField { (alertTextField) in
         alertTextField.placeholder = "Create new item"
-        textField = alertTextField
+        textField = alertTextField //<- because this has a pretty limited scope.
 
         }
         
